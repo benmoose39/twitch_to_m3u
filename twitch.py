3bd20d140755d53e022978e21c47e65f5e9e940d
benmoose39 <unnik1998@gmail.com>
2025-05-21 00:36:37 +0530

    Update twitch.py

--- twitch.py
+++ twitch.py
@@ -24,8 +24,8 @@ def getm3u(streamer):
     s.headers['User-Agent'] = random.choice(USER_AGENTS)
     response = s.get(f'{url}{streamer}').json()
     print(f'{response}')
-    print('sleeping for 10 seconds...')
-    time.sleep(10)
+    print('sleeping for 5 seconds...')
+    time.sleep(5)
     links = response['urls']
     qualities = {key.replace('p','') if '_' not in key else str(int(key.split('p_')[0])-1) : key for key in links.keys() if key != 'audio_only'}
 
